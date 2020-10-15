5b1ddfc9b3e94ef7b3bb9a029c18fee438214e97
Aregawi Halefom <aregawihalefom90@gmail.com>
2020-10-15 05:21:31 +0200

    CIFAR-10 LAB4_2

--- lab4_2_skeleton.py
+++ lab4_2_skeleton.py
@@ -1,290 +1,311 @@
 from __future__ import print_function
 
+# The two folloing lines allow to reduce tensorflow verbosity
+import os
+
+os.environ[
+    'TF_CPP_MIN_LOG_LEVEL'] = '1'  # '0' for DEBUG=all [default], '1' to filter INFO msgs, '2' to filter WARNING
+# msgs, '3' to filter all msgs
+
 import tensorflow as tf
 import tensorflow.keras
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
 from tensorflow.keras.optimizers import RMSprop, SGD, Adam
-from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
 
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 
 print('tensorflow:', tf.__version__)
 print('keras:', tensorflow.keras.__version__)
 
-##Uncomment the following two lines if you get CUDNN_STATUS_INTERNAL_ERROR initialization errors.
-## (it happens on RTX 2060 on room 104/moneo or room 204/lautrec) 
-# physical_devices = tf.config.experimental.list_physical_devices('GPU')
-# tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-# load (first download if necessary) the CIFAR10 dataset
-# data is already split in train and test datasets
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
-
-# Let start our work: creating a convolutional neural network
-
-#####TO COMPLETE
-
-def processed_data(x_train, x_test):
+# load and prepocess data
+def load_and_process_data():
     """
-    - Loads cifar 10 data
-    - Normalize the data
-    - change it to flot for computational reasons
+    This model loads the MNIST dataset from the keras api
+    @ return : x_train : training features
+               y_train : training labels
+               x_test  : test features
+               y_test : test labels
+               x_val : validation features
+               y_val : validation labels
+
     """
+    num_classes = 10
+
+    # load dataset
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    print('y_train.shape=', y_train.shape)
+    print('y_test.shape=', y_test.shape)
+
+    # Convert class vectors to binary class matrices ("one hot encoding")
+    ## Doc : https://keras.io/utils/#to_categorical
+    y_train = tensorflow.keras.utils.to_categorical(y_train)
+    y_test = tensorflow.keras.utils.to_categorical(y_test)
+
+    # Convert to float
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
+    print('x_train.shape=', x_train.shape)
+    print('x_test.shape=', x_test.shape)
 
+    # Normalize inputs from [0; 255] to [0; 1]
     x_train = x_train / 255
     x_test = x_test / 255
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, train_size=0.8)
 
-    return x_train, x_test
-
-def dataAugmentation(x_train,x_val,x_test,y_train=None,show_flag=False):
-
-    train_generator = ImageDataGenerator(
-    rotation_range=15,
-    horizontal_flip=True,
-    width_shift_range=0.1,
-    height_shift_range=0.1)
-
-    val_generator = ImageDataGenerator(
-    rotation_range=15,
-    horizontal_flip=True,
-    width_shift_range=0.1,
-    height_shift_range=0.1)
-
-    test_generator = ImageDataGenerator(
-    rotation_range=15,
-    horizontal_flip=True,
-    width_shift_range=0.1,
-    height_shift_range=0.1)
-
-    train_generator.fit(x_train)
-    val_generator.fit(x_val)
-    test_generator.fit(x_test)
-    
-    
-    if show_flag:
-        for x, y in train_generator.flow(x_train, y_train, batch_size=25):
-            for i in range(0, 24):
-                plt.subplot( 5,5, 1 + i)
-                plt.imshow(x[i],cmap=plt.cm.binary)
-            plt.show()
-            break
-    return train_generator,val_generator,test_generator
+    return x_train, y_train, x_test, y_test, x_val, y_val
 
 
-
-def visualize_sample_images(class_names, x_train, y_train):
-    plt.figure(figsize=(10, 10))
-    for i in range(12):
-        plt.subplot(3, 4, i + 1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.grid(False)
-        plt.imshow(x_train[i], cmap=plt.cm.binary)
-        # The CIFAR labels happen to be arrays,
-        # which is why you need the extra index
-        plt.xlabel(class_names[y_train[i][0]])
+# define label names
+def load_label_names():
+    """
+    Returns the labels in a string fromat for 10 classes of CIFAR-10
+    """
+    return ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 
-def model1():
-    # creating model instance
-    model1 = Sequential()
+# draw the training statistics
+def plot_training_metrics(choice, history):
+    """
+    This function plots the training metrics statistics
 
-    # first Convolutuional layers
-    model1.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(32, 32, 3)))
+    """
 
-    # Maxpooling Layers
-    model1.add(MaxPooling2D(2, 2))
-
-    model1.add(Dropout(0.25))
-    model1.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
-
-    model1.add(MaxPooling2D(2, 2))
-    model1.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform'))
-
-    model1.add(MaxPooling2D(2, 2))
-
-    # Flatten Layers
-    model1.add(Flatten())
-
-    # Linear Layer
-    model1.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
-
-    # Final Layer with softmax activation
-    model1.add(Dense(10, activation='softmax'))
-
-    # complile model
-    optm = SGD(lr=0.01, momentum=0.25)
-    model1.compile(optimizer=optm, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-    return model1
-
-def Model2():
-
-    model2 = Sequential()
-    model2.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', kernel_initializer='he_uniform',
-                      input_shape=(32, 32, 3)))
-    model2.add(MaxPooling2D(pool_size=(2, 2)))
-    model2.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', kernel_initializer='he_uniform'))
-    model2.add(MaxPooling2D(pool_size=(2, 2)))
-    model2.add(Flatten())
-    model2.add(Dense(256, activation='relu'))
-    model2.add(Dense(10, activation='softmax'))
-
-    optm = Adam(learning_rate=0.05)
-    model2.compile(optimizer=optm, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-    return model2
-
-
-def Model3():
-    model3 = Sequential()
-    model3.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', kernel_initializer='he_uniform',
-                      input_shape=(32, 32, 3)))
-    model3.add(MaxPooling2D(pool_size=(2, 2)))
-    model3.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', kernel_initializer='he_uniform'))
-    model3.add(MaxPooling2D(pool_size=(2, 2)))
-    model3.add(Flatten())
-    model3.add(Dense(256, activation='relu'))
-    model3.add(Dense(10, activation='softmax'))
-
-    optm = Adam(learning_rate=0.04)
-    model3.compile(optimizer=optm, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-    return model3
-
-
-def train_model(model, x_train, y_train, batch_size, epochs, validation_split):
-    hist = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=validation_split)
-
-    return hist
-
-
-def plot_training_metrics(choosen_model, history):
-    # directory to save the plots
-    path = os.path.join('results', str(choosen_model))
-    if not os.path.exists(path):
-        os.mkdir(path)
     # plotting the metrics
     fig = plt.figure(1)
     plt.plot(history.history['accuracy'])
     plt.plot(history.history['val_accuracy'])
+
     plt.title('model accuracy')
     plt.ylabel('accuracy')
+
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='lower right')
-    plt.savefig('{}/accuracy-model-{}.png'.format(path, choosen_model), format='png')
 
     plt.figure(2)
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
+
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
+
     plt.legend(['train', 'val'], loc='upper right')
     plt.tight_layout()
-    plt.savefig('{}/loss-model-{}.png'.format(path, choosen_model), format='png')
 
 
-def evaluate_model(model, X_test, Y_test):
-    evaluation_metrics = model.evaluate(X_test, Y_test, verbose=2)
-    print("Test Loss", evaluation_metrics[0])
-    print("Test Accuracy", evaluation_metrics[1])
-
-    return evaluation_metrics
-
-
+# visualize  wrong predictions
 def visualize_correct_and_wrong_predictions(choosen_model, model, X_test, y_test):
-    path = os.path.join('results', str(choosen_model))
-    print(path)
-    if not os.path.exists(path):
-        os.mkdir(path)
+
+    """
+    This function shows wrongly classified sample images
+    @params : model , name of model , test data
+    @return : none
+    """
+
+    #1. predicted classes
     predicted_classes = model.predict_classes(X_test)
+
+    #2. reverse the true level from one hot encoding back  to normal ture lavel
     y_reversed = np.argmax(y_test, axis=1)
-    # see which we predicted correctly and which not
+
+    #3. see which we predicted correctly and which not
     correct_indices = np.nonzero(predicted_classes == y_reversed)[0]
     incorrect_indices = np.nonzero(predicted_classes != y_reversed)[0]
+
     print()
     print(correct_indices.shape[0], " classified correctly")
     print(incorrect_indices.shape[0], " classified incorrectly")
 
-    # adapt figure size to accomodate 18 subplots
+    # label names
+    actual_classes = load_label_names()
 
-    plt.figure(3)
-    wrong_nine = incorrect_indices[:18]
+    # adapt figure size to accomodate 18 subplots
+    plt.figure(figsize=(10, 10))
+
+    # get wrong 9 samples
+    wrong_nine = incorrect_indices[:9]
+
     # plot 9 incorrect predictions
     for i, incorrect in enumerate(wrong_nine):
-        plt.subplot(6, 3, i + 1)
-        plt.imshow(X_test[incorrect].reshape(28, 28), cmap='gray', interpolation='none')
+
+        plt.subplot(3, 3, i + 1)
+        plt.imshow(X_test[incorrect].reshape(32, 32, 3), cmap='gray', interpolation='none')
         plt.title(
-            "Predicted = {}".format(predicted_classes[incorrect]))
+            "True = {} \n Predicted = {}".format(actual_classes[y_reversed[incorrect]],
+                                                 actual_classes[predicted_classes[incorrect]]))
         plt.xticks([])
         plt.yticks([])
 
-    plt.savefig('{}/wrongly-classified-model-{}.png'.format(path, choosen_model), format='png')
-    plt.tight_layout()
+    plt.tight_layout(pad=1)
+    # plt.savefig('{}/wrongly-classified-model-{}.png'.format(path, choosen_model), format='png')
+    # plt.clf()
 
 
+# save model
 def save_model(model, model_name):
+    """
+     Saves model as .h5 file format
+     @params : model , model name
+     @ return : None
+    """
     # saving the model
-    save_dir = "results/"
+    save_dir = "results/cifar10/" + str(model_name)
+
     if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
-    model_name = model_name + '.h5'
-    model_path = os.path.join(save_dir, model_name)
+        os.makedirs(save_dir)
+    file_name = str(model_name) + '.h5'
+    model_path = os.path.join(save_dir, file_name)
     model.save(model_path)
     print('Saved trained model at %s ' % model_path)
 
 
-if __name__ == '__main__':
-    # load dataset from the keras api
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+# First baseline model
+def Model1(lr):
+    """
+     First baseline model ( simpler one)
+     architecture : conv -> max-pooling -> flatten -> dense -> dense -> dense
 
-    # visualize sample images
-    class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-                   'dog', 'frog', 'horse', 'ship', 'truck']
-    # download and preprocess image
-    x_train, x_test = processed_data(x_train, x_test)
+    """
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(3, 3),
+                     activation='relu',
+                     kernel_initializer='he_uniform',
+                     input_shape=(32, 32, 3)))
+    model.add(MaxPooling2D(2, 2))
 
-    # visualize sample images
-    #visualize_sample_images(class_names, x_train, y_train)
-    #Do data Augmentation
-    generator=dataAugmentation(x_train,x_train,x_test,y_train,True)
-    
-    choose_model = 1
-    # crate the model
-    if choose_model == 1:
-        model = model1()
-        model_name = 'cifar-cnn-1'
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(10, activation='softmax'))
 
-    elif choose_model == 2:
-        model = Model2()
-        model_name = 'cifar-cnn-2'
-    elif choose_model == 3:
-        model = Model3()
-        model_name = 'cifar-cnn-3'
+    optm = SGD(lr=lr, momentum=0.9)
+    model.compile(optimizer=optm, loss='categorical_crossentropy', metrics=['accuracy'])
 
-    # train the model
-    # Hyper parameters
+    return model
+
+
+# Second model
+def Model2(lr):
+    """
+    Second model with more convolutional layers and
+    without any regularaization
+    architecture : conv -> conv -> max-pooling -> conv -> conv -> max-pooling ->flatten -> dense -> dense
+    """
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(32, 32, 3)))
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2, 2)))
+
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(10, activation='softmax'))
+
+    # compile model
+    opt = SGD(lr=lr, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+# Third model
+def Model3(lr):
+    """
+     Third model with more convolutional layers and
+     with  regularaization
+     Architecture : conv -> conv -> max-pooling -> conv -> conv -> max-pooling ->dropout-> flatten -> dense -> dropout->dense
+
+    """
+
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(32, 32, 3)))
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2, 2)))
+
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.4))
+
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dropout(0.4))
+    model.add(Dense(10, activation='softmax'))
+
+    # compile model
+    opt = SGD(lr=lr, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+# Selects specific model
+def select_model(choice, lr):
+    """
+    Creates model instance according to choice
+    @params : choice
+    @ return : model
+    """
+    model = None
+    if choice == 1:
+        model = Model1(lr)
+    elif choice == 2:
+        model = Model2(lr)
+    elif choice == 3:
+        model = Model3(lr)
+
+    return model
+
+
+# Function to Run the model
+def run_program():
+
+    ###################################
+    #     Program starts Here         #
+    ###################################
+
+    # 1. load the dataset
+    x_train, y_train, x_test, y_test, x_val, y_val = load_and_process_data()
+
+    # 2. Define model
+    #####################################3
+    # Hyperparameters
     batch_size = 128
-    epochs = 20
-    validation_split = 0.2
-    history = train_model(model, x_train, y_train, batch_size=batch_size, epochs=epochs,
-                          validation_split=validation_split)
+    epochs = 50
+    lr = 0.01
+    ######################################
 
-    # visualize training statistics
-    plot_training_metrics(choose_model, history)
+    choice = 3  # 1, 2, or 3
+    model = select_model(choice, lr)
+    model.summary()
 
-    # evaluate the model
-    evaluation_metrics = evaluate_model(model, x_test, y_test)
+    # 3. train model
+    hist = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_val, y_val))
 
-    # save the model
-    save_model(model, model_name)
+    # 4. display training details
+    plot_training_metrics(choice, hist)
 
+    # 5. evaluate model
+    loss, acc = model.evaluate(x_test, y_test, verbose=2)
+    print("Test Loss: ", loss)
+    print("Test Accuracy", acc)
+
+    # 6. save model
+    #save_model(model, choice)
+
+    #7. show plots if any
     plt.show()
+
+
+if __name__ == "__main__":
+
+    # ran everthing from here
+    run_program()
+
+
